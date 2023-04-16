d48bb85d45faea4628b701d4d600aef6310d1197
Bohdan <radchenkobs@gmail.com>
2023-04-16 17:38:50 +0300

    create mock test data for debug ci gh actions

---
+++ src/test/java/TestCaseForDebug.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCaseForDebug {
+    @Test
+    public void testCase() {
+        assertEquals("Test", 0, 0);
+    }
+}
