4c6f92fb884659bf5d52f2cd2ddb1f01bbb65f59
Bohdan <radchenkobs@gmail.com>
2023-04-16 19:23:01 +0300

    clear project/ initial for dev

--- src/main/java/org/booking/Main.java
+++ src/main/java/org/booking/Main.java
@@ -1,10 +1,7 @@
 package org.booking;
 
-import org.booking.libs.logger.Logger;
-
 public class Main {
     public static void main(String[] args) {
-        Logger.start();
         System.out.println("Hello world!");
     }
 }
