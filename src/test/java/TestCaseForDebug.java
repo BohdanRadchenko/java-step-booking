fdb5cd8aaf6f0ccb9845cb157645698deaa3ceb2
Bohdan <radchenkobs@gmail.com>
2023-04-16 19:16:25 +0300

    debug test/ run pre commit hook

--- src/test/java/TestCaseForDebug.java
+++ src/test/java/TestCaseForDebug.java
@@ -5,6 +5,6 @@ import static org.junit.Assert.assertEquals;
 public class TestCaseForDebug {
     @Test
     public void testCase() {
-        assertEquals("Test", 0, 0);
+        assertEquals("Test", 2, 2);
     }
 }
