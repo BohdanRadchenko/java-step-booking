d5d996efc0c858331b3858a366b5cc2e1f364194
Bohdan <radchenkobs@gmail.com>
2023-04-19 01:13:52 +0300

    refactor parser mathcer method

--- src/main/java/org/booking/libs/Parser.java
+++ src/main/java/org/booking/libs/Parser.java
@@ -4,15 +4,19 @@ import java.util.Arrays;
 import java.util.Set;
 
 public class Parser {
-    private static final Set<String> exitWords = Set.of(new String[]{"exit", "ex", "e", "quit", "q"});
-    private static final Set<String> backWords = Set.of(new String[]{"back", "b"});
+    private static final Set<String> exitWords = Set.of("exit", "ex", "e", "quit", "q");
+    private static final Set<String> backWords = Set.of("back", "b");
+
+    private static boolean matcher(String str, Set<String> matchWords) {
+        return Arrays.stream(str.split(" ")).anyMatch(matchWords::contains);
+    }
 
     public static boolean parseIsExit(String str) {
-        return Arrays.stream(str.split(" ")).anyMatch(exitWords::contains);
+        return matcher(str, exitWords);
     }
 
     public static boolean parseIsBack(String str) {
-        return Arrays.stream(str.split(" ")).anyMatch(backWords::contains);
+        return matcher(str, backWords);
     }
 
     public static int parseInt(String str) throws NumberFormatException {
