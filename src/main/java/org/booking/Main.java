d6c9f06744073347a798d31f792678833ab05689
Vladimirov2077 <131011605+Vladimirov2077@users.noreply.github.com>
2023-04-18 20:30:35 +0200

    004 create main booking manager class with started methods

--- src/main/java/org/booking/Main.java
+++ src/main/java/org/booking/Main.java
@@ -2,6 +2,7 @@ package org.booking;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BookingManager bookingManager = new BookingManager();
+        bookingManager.run();
     }
 }
