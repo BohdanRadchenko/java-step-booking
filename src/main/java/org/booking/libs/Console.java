e63a006c9ca3f2526cea3e9b188462c5d005d70b
Vladimirov2077 <131011605+Vladimirov2077@users.noreply.github.com>
2023-04-19 13:40:31 +0200

    SB-005 Create Console out class (#9)

---
+++ src/main/java/org/booking/libs/Console.java
@@ -0,0 +1,38 @@
+package org.booking.libs;
+
+public final class Console extends Input {
+
+    public static void print(String str) {
+        System.out.print(str);
+    }
+
+    public static void reset() {
+        print(ConsoleColors.RESET);
+    }
+
+    public static void ln() {
+        System.out.println();
+    }
+
+    public static void error(String msg) {
+        print(ConsoleColors.RED);
+        print(msg);
+        reset();
+    }
+
+    public static void input(String msg) {
+        print(ConsoleColors.BLUE_UNDERLINED);
+        print(msg);
+        reset();
+    }
+
+    public static void log(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void warning(String msg) {
+        print(ConsoleColors.YELLOW);
+        print(msg);
+        reset();
+    }
+}
