b269d5180baade8daefcec0ed35aca79384797a9
Bohdan <radchenkobs@gmail.com>
2023-04-19 23:35:18 +0300

    save fligh info

--- src/main/java/org/booking/helpers/MenuDescription.java
+++ src/main/java/org/booking/helpers/MenuDescription.java
@@ -3,9 +3,8 @@ package org.booking.helpers;
 public class MenuDescription {
     public static String flightInfo() {
         String row1 = "Enter the FLIGHT ID in AAxxxx format.\n";
-        String row2 = "AA - airline\n";
-        String row3 = "xxxx - flight number\n";
-        String row4 = "or 'Back' to main menu";
-        return String.format("%s%s%s%s", row1, row2, row3, row4);
+        String row2 = "AA - airline, xxxx - flight number.\n";
+        String row3 = "or 'Back' to main menu";
+        return String.format("%s%s%s", row1, row2, row3);
     }
 }
