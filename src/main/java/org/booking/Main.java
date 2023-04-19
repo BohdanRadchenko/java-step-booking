bf80832d6facffbcba794fe538c3bb343555615d
Bohdan <radchenkobs@gmail.com>
2023-04-19 20:35:17 +0300

    056 create airline

--- src/main/java/org/booking/Main.java
+++ src/main/java/org/booking/Main.java
@@ -1,8 +1,11 @@
 package org.booking;
 
+import org.booking.enums.Airline;
+
 public class Main {
     public static void main(String[] args) {
         BookingManager bookingManager = new BookingManager();
         bookingManager.run();
     }
+
 }
