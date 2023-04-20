7f14e8c75e2d25ed3b65f596f3f23206f69239ab
Bohdan Radchenko <49039878+BohdanRadchenko@users.noreply.github.com>
2023-04-20 11:37:01 +0300

    038 create basic controller (#19)

--- src/main/java/org/booking/ui/menu/MainMenu.java
+++ src/main/java/org/booking/ui/menu/MainMenu.java
@@ -1,11 +1,12 @@
 package org.booking.ui.menu;
 
 import org.booking.command.Login;
+import org.booking.controllers.Controller;
 import org.booking.enums.MenuName;
 
 public class MainMenu extends Menu {
-    public MainMenu() {
+    public MainMenu(Controller controller) {
         super("Main menu");
-        add(1, MenuName.LOGIN, Login.of());
+        add(1, MenuName.LOGIN, Login.of(controller));
     }
 }
