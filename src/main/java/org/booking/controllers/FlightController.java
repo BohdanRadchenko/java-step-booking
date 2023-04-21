b66c5445332b157de6379c0056fb617a5acbec01
Simon <69726466+SimonPe25@users.noreply.github.com>
2023-04-21 14:22:59 +0300

    Sb 052 create basic flight entity (#21)
    Flight whith speed and seats
    
    ---------
    
    Co-authored-by: Bohdan <radchenkobs@gmail.com>

--- src/main/java/org/booking/controllers/FlightController.java
+++ src/main/java/org/booking/controllers/FlightController.java
@@ -1,17 +1,56 @@
 package org.booking.controllers;
 
+import org.booking.entity.Aircraft;
+import org.booking.entity.Airline;
+import org.booking.entity.Airport;
 import org.booking.entity.Flight;
+import org.booking.enums.FilePath;
 import org.booking.interfaces.IController;
 import org.booking.services.ServiceFlight;
+import org.booking.utils.FileWorker;
+import org.booking.utils.Randomize;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FlightController implements IController {
     private final ServiceFlight service = new ServiceFlight();
 
+    private long generateTime() {
+        return System.currentTimeMillis();
+    }
+
+    private List<Flight> generateFlights(int count) {
+        // TODO: 21.04.2023 MVP1
+        // TODO: 21.04.2023 create time generator
+
+        // TODO: 21.04.2023 MVP2
+        // TODO: 21.04.2023 create airport from generator
+        // TODO: 21.04.2023 create airport to generator
+        // TODO: 21.04.2023 create flight id generator
+        // TODO: 21.04.2023 create generator with MVP2 task
+
+        List<Flight> flights = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            int id = Randomize.num(999);
+            Aircraft aircraft = Aircraft.values()[Randomize.num(Aircraft.values().length)];
+            Airline airline = Airline.values()[Randomize.num(Airline.values().length)];
+            int fromIdx = 0;
+            Airport from = Airport.values()[fromIdx];
+            Airport to = Airport.values()[Randomize.num(0, Airport.values().length, fromIdx)];
+
+            flights.add(new Flight(generateTime(), from, to, airline, aircraft, id));
+        }
+        return flights;
+    }
+
     @Override
     public void load() throws RuntimeException {
-        // TODO: 20.04.2023 load data from file. If file is not exist - generate data
+        if (!FileWorker.exist(FilePath.FLIGHT)) {
+            service.upload(generateFlights(100));
+            return;
+        }
+        // TODO: 20.04.2023 load data from file.
         ArrayList<Flight> f = new ArrayList<>();
         service.upload(f);
     }
--- src/main/java/org/booking/entity/Flight.java
+++ src/main/java/org/booking/entity/Flight.java
@@ -1,4 +1,55 @@
 package org.booking.entity;
 
+import org.booking.helpers.Utm;
+
+import java.util.*;
+
 public class Flight extends Entity {
+    public long departureTimeStamp;
+    public long arrivalTimeStamp;
+    private Airport departureAirport;
+    private Airport arrivalAirport;
+    private Airline airline;
+    private Aircraft aircraft;
+    private String flightId;
+    private List<String> reserved;
+    private int freeSeats;
+
+
+    public Flight(long departureTimeStamp, Airport departureAirport, Airport arrivalAirport, Airline airline,
+                  Aircraft aircraft, int flightId, List<String> reserved) {
+        this.departureTimeStamp = departureTimeStamp;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.airline = airline;
+        this.flightId = String.format("%03d", flightId);
+        this.reserved = reserved;
+        this.aircraft = aircraft;
+        this.freeSeats = aircraft.seats - reserved.size();
+        this.arrivalTimeStamp = arrivalTimeMls();
+    }
+
+    public Flight(long departureTimeStamp, Airport departureAirport, Airport arrivalAirport,
+                  Airline airline, Aircraft aircraft, int flightId) {
+        this(departureTimeStamp, departureAirport, arrivalAirport, airline, aircraft, flightId, new ArrayList<>());
+    }
+
+    private long arrivalTimeMls() {
+        int distance = Utm.distance(departureAirport, arrivalAirport);
+        int cruiserTime = distance / aircraft.speed * 60 * 60 * 100;
+        return this.departureTimeStamp + cruiserTime + 3600000;
+    }
+
+    public int getFreeSeats() {
+        return freeSeats;
+    }
+
+    //test
+    public long getDepartureTimeStamp() {
+        return this.departureTimeStamp;
+    }
+
+    public long getArrivalTimeStamp() {
+        return this.arrivalTimeStamp;
+    }
 }
