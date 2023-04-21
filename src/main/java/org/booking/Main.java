75604ab7db3ca6f1344449b1cb72a830293af46e
Simon <simpla@ukr.net>
2023-04-21 15:01:50 +0300

    test

--- src/main/java/org/booking/Main.java
+++ src/main/java/org/booking/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         BookingManager bookingManager = new BookingManager();
         bookingManager.run();
+        System.out.println("Test");
     }
 }
